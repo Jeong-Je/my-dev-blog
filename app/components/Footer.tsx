3d767f3280c3f7f3ead9a82c5a0bdedd72250e2d
Jeong-Je <jeongjeson656@gmail.com>
2023-08-27 23:34:39 +0900

    Add Footer Layout 🦶

---
+++ app/components/Footer.tsx
@@ -0,0 +1,35 @@
+import {
+  AiOutlineGithub,
+  AiOutlineMail,
+} from "react-icons/ai";
+
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <div className="max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{" "}
+            JeongJeSon
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li className="hover:underline md:ml-5">
+            <a href="/about">
+              About
+            </a>
+          </li>
+          <li className="ml-5">
+            <a href="mailto:jeongjeson656@gmail.com">
+              <AiOutlineMail size={20}/>
+            </a>
+          </li>
+          <li className="ml-5">
+            <a href="https://github.com/Jeong-Je" target="_blank">
+              <AiOutlineGithub size={20} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
