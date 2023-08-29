30babb28a48697de0d5938f02d2213b4acfd5051
Jeong-Je <jeongjeson656@gmail.com>
2023-08-29 15:06:25 +0900

    Modify layout ✂️

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,6 +2,7 @@
 import { format, parseISO } from "date-fns";
 import { allPosts } from "contentlayer/generated";
 import { useMDXComponent } from "next-contentlayer/hooks";
+import Link from "next/link";
 
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
@@ -9,7 +10,7 @@ export const generateStaticParams = async () =>
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
   if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
-  return { title: post.title };
+  return { title: post.title, description: post.description };
 };
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
@@ -18,19 +19,32 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
   const tags = post.tags;
   const MDXContent = useMDXComponent(post.body.code);
   return (
-    <article className="pt-24 mx-auto prose prose-a:bg-green-400 prose-a:no-underline prose-blockquote:border-l-emerald-400 max-sm:mx-5">
-      <div className="flex items-center">
-        <h1 className="pt-5 text-5xl font-bold">{post.title}</h1>
-        <time dateTime={post.date} className="ml-2 text-xs text-gray-600">
-          {format(parseISO(post.date), "LLLL d, yyyy")}
-        </time>
-        <br />
-      </div>
-      <MDXContent />
-      {tags?.map((tag, idx) => (
-              <button className="rounded-full border border-gary-500 bg-gray-200 text-black text-sm px-4 mr-2" key={idx}>{tag}</button>
-            ))}
-    </article>
+    <>
+      {/* 본문 (추후 구조 수정 필요) */}
+      <article className="pt-24 mx-auto prose prose-a:bg-green-400 prose-a:no-underline prose-blockquote:border-l-emerald-400 max-sm:mx-5">
+        <div className="flex">
+          <h1 className="pt-5 pb-0 text-4xl font-bold">
+            {post.title}
+            <time dateTime={post.date} className="ml-2 text-xs text-gray-400">
+              {format(parseISO(post.date), "LLLL d, yyyy")}
+            </time>
+            <div className="prose-a:bg-white">
+          {tags?.map((tag, idx) => (
+            <Link href={`/posts/?tag=${tag}`}>
+              <button
+                className="rounded-full border border-gary-500 bg-gray-200 hover:bg-gray-400 text-black text-sm px-4 mr-2"
+                key={idx}
+              >
+                {tag}
+              </button>
+            </Link>
+          ))}
+        </div>
+          </h1>
+        </div>
+        <MDXContent />
+      </article>
+    </>
   );
 };
 
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,31 +1,46 @@
-import Link from 'next/link'
-import { compareDesc, format, parseISO } from 'date-fns'
-import { allPosts, Post } from 'contentlayer/generated'
+import Link from "next/link";
+import { compareDesc, format, parseISO } from "date-fns";
+import { allPosts, Post } from "contentlayer/generated";
 
 function PostCard(post: Post) {
   return (
-    <div className="mb-8 prose">
-      <h2 className="mb-1 text-xl">
-        <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
-          {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(post.date), 'LLLL d, yyyy')}
-      </time>
-      <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.description }} />
+    <div className="mb-8 prose max-sm:mx-5">
+      <Link
+        href={post.url}
+        className="text-black no-underline hover:font-bold dark:text-blue-400"
+      >
+        <h2 className="mb-1 text-xl">{post.title}</h2>
+        <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+          {format(parseISO(post.date), "LLLL d, yyyy")}
+        </time>
+        <div
+          className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
+          dangerouslySetInnerHTML={{ __html: post.description }}
+        />
+      </Link>
+      <div>
+        {post.tags?.map((tag, idx) => (
+          <Link href={`/posts/?tag=${tag}`} key={idx}>
+            <button className="rounded-full border border-gary-500 bg-gray-200 hover:bg-gray-400 text-black text-xs px-4 mr-2">
+              {tag}
+            </button>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default function Home() {
-  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
-
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  );
+  console.log(posts[2].tags);
   return (
     <div className="pt-24 py-8 w-[650px] mx-auto">
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
       ))}
     </div>
-  )
+  );
 }
