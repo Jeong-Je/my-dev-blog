dea5663c88c6b830c667906b6b534e7fddb65aba
Jeong-Je <jeongjeson656@gmail.com>
2023-11-22 17:16:47 +0900

    Update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,38 @@
-import { IoBulbOutline } from "react-icons/io5";
-import { SiTailwindcss } from "react-icons/si";
-
 export default function Home() {
   return (
-    <div className="pt-32 mx-auto prose prose-a:no-underline max-sm:mx-5">
+    <div className="prose pt-32 mx-auto max-sm:mx-5">
       <div className="rounded-lg p-4 bg-gray-200">
         ℹ️ 이 곳은 제가 공부한 내용을 정리하고 기록하는 공간입니다.🚀✨
       </div>
+      <hr />
+      <h3>🖥️ 주요 관심사항</h3>
+      <ul>
+        <li>Web (mainly backend)</li>
+        <li>Windows System Programming</li>
+        <li>Network</li>
+        <li>Database</li>
+      </ul>
+      <hr />
+      {/* <h3>💡 해당 블로그는 아래 기술들로 만들어졌습니다.</h3> */}
 
-      <br />
-      <br />
-      <br />
-
-      <div className="flex items-center">
-        <IoBulbOutline className="text-amber-300 text-5xl" />
-        <p className="ml-2">해당 블로그는 아래 기술들로 만들어졌습니다.</p>
-      </div>
-      <SiTailwindcss color="#06B6D4" size={50} />
-      
-
+      {/* <div className="flex items-center h-5">
+        <a href="https://nextjs.org/" target="_blank" className="w-20">
+          <img src="https://img.shields.io/badge/NextJS-000000?style=flat&logo=nextdotjs&logoColor=white" />
+        </a>
+        <a
+          href="https://www.typescriptlang.org/"
+          target="_blank"
+          className="w-20"
+        >
+          <img src="https://img.shields.io/badge/TypeScript-3178C6?style=flat&logo=typescript&logoColor=white" />
+        </a>
+        <a href="https://tailwindcss.com/" target="_blank" className="w-20">
+          <img src="https://img.shields.io/badge/TailwindCSS-06B6D4?style=flat&logo=tailwindcss&logoColor=white" />
+        </a>
+        <a href="https://vercel.com/" target="_blank" className="w-20">
+          <img src="https://img.shields.io/badge/Vercel-000000?style=flat&logo=vercel&logoColor=white" />
+        </a>
+      </div> */}
     </div>
   );
 }
